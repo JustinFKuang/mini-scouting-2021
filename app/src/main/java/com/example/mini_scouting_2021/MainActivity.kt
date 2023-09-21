4cf39b19b55b10eee957f2eafc3c6c1156d692e0
Justin <3561898074kk@gmail.com>
2023-09-20 20:36:41 -0700

    subbbmit

---
+++ app/src/main/java/com/example/mini_scouting_2021/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mini_scouting_2021
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.match_input_activity)
+    }
+}
